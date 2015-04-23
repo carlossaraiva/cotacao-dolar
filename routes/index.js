dd66debd89e3247676dd037bbc243943bbd44e01
Carlos Saraiva <carlos.saraiva@msn.com>
2015-04-23 04:24:42 +0100

    /json path added

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,20 @@
 var express = require('express');
 var router = express.Router();
+var Cotacao = require('../models/cotacao.js');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
+});
+
+router.get('/json', function(req, res, next){
+	Cotacao.find(function(err, cotacaos){
+		if(!err){
+			res.send(cotacaos);
+		}else{
+			res.send("Not found");
+		}
+	});
 });
 
 module.exports = router;
