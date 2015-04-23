9d9be146f2496f7f1a7dda002c1a9f9f3c259801
Carlso Saraiva <carlos.saraiva@msn.com>
2015-04-23 13:48:26 -0300

    robot.js path fixed

--- routes/index.js
+++ routes/index.js
@@ -4,7 +4,7 @@ var Cotacao = require('../models/cotacao.js');
 
 /* GET home page. */
 router.get('/', function (req, res, next) {
-
+    "use strict";
     res.render('index', {
         title: 'Express'
     });
