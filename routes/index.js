6a53a5d19d9943f71ab93a8845b0e7ad03716ca5
Carlos Saraiva <carlos.saraiva@msn.com>
2015-04-24 20:33:24 +0100

    /json can now return json by date order with date post data

--- routes/index.js
+++ routes/index.js
@@ -4,17 +4,26 @@ var Cotacao = require('../models/cotacao.js');
 
 /* GET home page. */
 router.get('/', function (req, res, next) {
-  res.render('index', { title: 'Express' });
+
+    Cotaca.find(function (err, cotacao) {
+        res.render('index', {
+            title: 'Cotaçãõ do Dolar - A robot example'
+        });
+    });
 });
 
-router.get('/json', function(req, res, next){
-	Cotacao.find(function(err, cotacoes){
-		if(!err){
-			res.jsonp(cotacoes);
-		}else{
-			res.jsonp([{"Error":"That's fucked up bro..." }]);
-		}
-	});
+router.post('/json', function (req, res, next) {
+    var sort = req.body.sortField + ':' + req.body.sortOrder;
+
+    Cotacao.find({}).sort(sort).find(function (err, cotacoes) {
+        if (!err) {
+            res.jsonp(cotacoes);
+        } else {
+            res.jsonp([{
+                "Error": "That's fucked up bro..."
+            }]);
+        }
+    });
 });
 
 module.exports = router;
