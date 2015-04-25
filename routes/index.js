777ab08087a3aa7d3e14df95d5ad296376372dc0
Carlos Saraiva <carlos.saraiva@msn.com>
2015-04-25 02:40:54 +0100

    View index implementada

--- routes/index.js
+++ routes/index.js
@@ -5,11 +5,13 @@ var Cotacao = require('../models/cotacao.js');
 /* GET home page. */
 router.get('/', function (req, res, next) {
 
-    Cotacao.find(function (err, cotacao) {
-        console.log(cotacao);
+    Cotacao.find({}).sort({
+        date: -1
+    }).find(function (err, cotacao) {
+        var last = cotacao[0];
         res.render('index', {
-            title: 'Cotaçãõ do Dolar - A robot example',
-            cotacaoList: cotacao
+            title: 'Cotação do Dolar',
+            cotacaoLast: last
         });
 
     });
@@ -17,8 +19,6 @@ router.get('/', function (req, res, next) {
 
 router.post('/json', function (req, res, next) {
     var sort = req.body.sortField + ':' + req.body.sortOrder;
-    console.log(sort);
-
     Cotacao.find({}).sort(sort).find(function (err, cotacoes) {
         if (!err) {
             res.jsonp(cotacoes);
