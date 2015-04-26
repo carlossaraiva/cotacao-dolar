e05dd39f99c9415f7cd868110122c24bfc307003
Carlos Saraiva <carlos.saraiva@msn.com>
2015-04-26 20:15:17 -0300

    Real time update with

--- public/js/index.js
+++ public/js/index.js
@@ -2,14 +2,21 @@ var socket = io();
 
 var app = angular.module('App', []);
 
-app.filter("moment", function () {
-    return function (input) {
-        return moment(input).fromNow();
+app.filter('moment', function () {
+    return function (input, momentFn) {
+        var args = Array.prototype.slice.call(arguments, 2),
+            momentObj = moment(input);
+        return momentObj[momentFn].apply(momentObj, args);
     };
 });
 
-app.controller("AppCtrl", function ($scope, $http) {
-    $http.get('/cotacao/json').success(function (data) {
-        $scope.dolar = data;
-    });
+app.controller("AppCtrl", function ($scope, $http, $timeout) {
+    var retrieve = function () {
+        $http.get('/cotacao/json').success(function (data) {
+            $scope.dolar = data;
+            $scope.dateNow = new Date();
+            $timeout(retrieve, 1000);
+        });
+    };
+    retrieve();
 });
